eb06ae16c1b3004a33bf50d2a2fad2fca70b02d9
Rudy Ochoa <rudy.ochoa.333@gmail.com>
2021-12-27 20:41:37 -0800

    Rinkby setup
    changes to get deployed on runkby

--- BlockchainCourse/starNotaryV2/ReadMe.txt
+++ BlockchainCourse/starNotaryV2/ReadMe.txt
@@ -2,4 +2,5 @@ ERC-721 Token Name: Rockstar
 ERC-721 Token Symbol: RSTR 
 Version of Truffle: 5.4.24
 Version of OpenZeppelin: 4.4.1
-Rinkeby Token Address: TODO:
+Rinkeby Token Address: 0x86E67b59b6dca8B46E81c9b17F6B017AE1574B19
+https://rinkeby.etherscan.io/address/0x86E67b59b6dca8B46E81c9b17F6B017AE1574B19
--- BlockchainCourse/starNotaryV2/truffle-config.js
+++ BlockchainCourse/starNotaryV2/truffle-config.js
@@ -18,11 +18,15 @@
  *
  */
 
- const HDWallet = require('truffle-hdwallet-provider');
+ //const HDWallet = require('truffle-hdwallet-provider');
+ const HDWalletProvider = require("@truffle/hdwallet-provider");
+ const Web3 = require("web3");
+ const infuraKey = "1a127ac1d17849e08f426b38330e5198";
 // const infuraKey = "fj4jll3k.....";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+//const fs = require('fs');
+//const mnemonic = fs.readFileSync(".secret").toString().trim();
+const mnemonic = "REDACTED";
 
 module.exports = {
   /**
@@ -55,7 +59,10 @@ module.exports = {
     },
 
     rinkeby: {
-
+      provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/1a127ac1d17849e08f426b38330e5198'),
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000
     }
 
     // Another network with more advanced options...
